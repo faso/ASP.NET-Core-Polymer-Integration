2b60efd17f1f4e8af000544acc1848d9193769ca
faso <yigbot@gmail.com>
2016-05-10 00:04:42 +0300

    Update HTML helpers

--- HTML Helpers/PolymerHtml.cs
+++ HTML Helpers/PolymerHtml.cs
@@ -1,13 +1,26 @@
 using Microsoft.AspNet.Html.Abstractions;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Routing;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Reflection;
 
 namespace System.Web.Mvc.Polymer
 {
+    public class PolymerTagBuilder : TagBuilder
+    {
+        public PolymerTagBuilder(string tagName)
+            : base(tagName)
+        { }
+
+        public PolymerTagBuilder(string tagName, object htmlAttributes)
+            : base(tagName)
+        {
+            if (htmlAttributes != null)
+                this.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+        }
+    }
+
     public static class PolymerHtml
     {
         private const string ironFormName = "iron-form";
@@ -22,7 +35,7 @@ namespace System.Web.Mvc.Polymer
 
         public static IHtmlContent BeginForm(string action = null, string method = null, object htmlAttributes = null)
         {
-            var builder = new TagBuilder("form");
+            var builder = new PolymerTagBuilder("form");
             builder.TagRenderMode = TagRenderMode.StartTag;
 
             builder.MergeAttribute("is", ironFormName);
@@ -33,10 +46,59 @@ namespace System.Web.Mvc.Polymer
             if (method != null)
                 builder.MergeAttribute("method", method);
 
-            if (htmlAttributes != null)
-                builder.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+            return builder;
+        }
+
+        public static IHtmlContent CheckBox(string label = null, bool check = false, object htmlAttributes = null)
+        {
+            var builder = new PolymerTagBuilder("paper-checkbox");
+            builder.TagRenderMode = TagRenderMode.Normal;
+
+            if (label != null)
+                builder.InnerHtml.AppendHtml(label);
+
+            if (check)
+                builder.MergeAttribute("checked", "");
 
             return builder;
+        }
+
+        public static IHtmlContent Input(string label = null, string prefix = null, string suffix = null, object htmlAttributes = null)
+        {
+            var builder = new PolymerTagBuilder("paper-checkbox");
+            builder.TagRenderMode = TagRenderMode.Normal;
+
+            if (prefix != null)
+            {
+                var pre = new TagBuilder("div");
+                pre.MergeAttribute("prefix", "");
+                pre.InnerHtml.AppendHtml(prefix);
+                builder.InnerHtml.Append(pre);
+            }
+
+            if (suffix != null)
+            {
+                var suf = new TagBuilder("div");
+                suf.MergeAttribute("suffix", "");
+                suf.InnerHtml.AppendHtml(suffix);
+                builder.InnerHtml.Append(suf);
+            }
+
+            if (label != null)
+                builder.InnerHtml.AppendHtml(label);
+
+            return builder;
+        }
+
+        public static IHtmlContent Password(string label = null, object htmlAttributes = null)
+        {
+            if (htmlAttributes == null)
+                htmlAttributes = new { type = "password" };
+            else
+            {
+                ((Dictionary<object, string>)htmlAttributes).Add("type", "password");
+            }
+            return PolymerHtml.Input(label, null, null, htmlAttributes);
         }
     }
 }
